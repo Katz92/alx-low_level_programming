fdde4a3285a35cd0b1b8d55aa577a8d0f9516564
Katz92 <katlegomagombo@gmail.com>
2023-03-20 02:44:42 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,13 @@
-#ifndef MAIN_C
-#define MAIN_C
+#include <stdio.h>
 
-#endif
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
