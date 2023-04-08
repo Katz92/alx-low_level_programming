fb3c5c7a33d341ee32ba69378f3e36a7a1c57413
Katz92 <katlegomagombo@gmail.com>
2023-04-08 09:58:01 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,33 +3,35 @@
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to print in binary
+ *
+ * Description: This function takes an unsigned long integer and prints its
+ * binary representation. It does this by first creating a mask to check the
+ * leftmost bit, then looping through each bit in the number and printing '1'
+ * if the bit is set and '0' if it is not set. If the number is zero, it prints
+ * a single '0'. The function does not return a value.
  */
 void print_binary(unsigned long int n)
 {
-    /* create a mask to check the leftmost bit */
-    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int found_first_bit = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int found_first_bit = 0;
 
-    while (mask > 0) {
-        /* check if the current bit is set */
-        if (n & mask)
-        {
-            found_first_bit = 1;
-            _putchar('1');
-        }
-        else if (found_first_bit)
-        {
-            _putchar('0');
-        }
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			found_first_bit = 1;
+			_putchar('1');
+		}
+		else if (found_first_bit)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
 
-        /* shift the mask to the right */
-        mask >>= 1;
-    }
-
-    /* if the number was 0, we need to print a single 0 */
-    if (!found_first_bit)
-    {
-        _putchar('0');
-    }
+	if (!found_first_bit)
+	{
+		_putchar('0');
+	}
 }
 
