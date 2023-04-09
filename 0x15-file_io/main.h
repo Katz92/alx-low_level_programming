19e841aef7bfe5881be84d232a567cee5f821ad9
Katz92 <katlegomagombo@gmail.com>
2023-04-09 12:54:04 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,55 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stddef.h> /* for size_t */
-
-/* function prototype */
-ssize_t read_textfile(const char *filename, size_t letters);
-
-#endif /* MAIN_H */
-
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * read_textfile - reads a text file and prints it to the POSIX stdout
- * @filename: name of the file to read
- * @letters: number of letters to read and print
- *
- * Return: actual number of letters read and printed, or 0 on failure
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	ssize_t n, m;
-	char *buf;
-	FILE *fp;
-
-	if (!filename)
-		return (0);
-	fp = fopen(filename, "r");
-	if (!fp)
-		return (0);
-	buf = malloc(sizeof(char) * (letters + 1));
-	if (!buf)
-	{
-		fclose(fp);
-		return (0);
-	}
-	n = fread(buf, sizeof(char), letters, fp);
-	if (n == -1)
-	{
-		free(buf);
-		fclose(fp);
-		return (0);
-	}
-	buf[n] = '\0';
-	m = printf("%s", buf);
-	free(buf);
-	fclose(fp);
-	if (m < 0)
-		return (0);
-	return (n);
-}
+#include "main.h"
 
 /**
  * main - check the code
@@ -71,4 +22,3 @@ int main(int ac, char **av)
 	printf("\n(printed chars: %li)\n", n);
 	return (0);
 }
-
