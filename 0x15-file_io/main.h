1b126090f85da94bff44f8b678d0d38eec3969a2
Katz92 <katlegomagombo@gmail.com>
2023-04-09 07:00:34 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,18 +2,11 @@
 #define MAIN_H
 
 /**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- * written in the 0x14-file_io directory.
- *
- * This file contains the function prototypes for the functions defined
- * in the file_io directory. These functions include reading from and
- * writing to files, creating files, and appending text to existing files.
+ *File: main.h
+ *Auth: Katlego Magombo
+ *Desc: Header file containing prototypes for all functions
+ *written in the 0x14-file_io directory.
  */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -21,17 +14,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/**
- * This header file contains the function prototypes for the functions defined
- * in the file_io directory. These functions include reading from and writing
- * to files, creating files, and appending text to existing files.
- */
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *str);
 
-#endif /* MAIN_H */
 
+#endif /*MAIN_H */
