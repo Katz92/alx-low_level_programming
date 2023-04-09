bbcd0483a0d7c3a907faacd630f76e50c38c327f
Katz92 <katlegomagombo@gmail.com>
2023-04-09 13:02:51 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * File: main.h
+ * Auth: Lerato Mthimkhulu
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x14-file_io directory.
  */
-int main(int ac, char **av)
-{
-	ssize_t n;
 
-	if (ac != 2)
-	{
-		dprintf(2, "Usage: %s filename\n", av[0]);
-		exit(1);
-	}
-	n = read_textfile(av[1], 114);
-	printf("\n(printed chars: %li)\n", n);
-	n = read_textfile(av[1], 1024);
-	printf("\n(printed chars: %li)\n", n);
-	return (0);
-}
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *str);
+
+#endif /* MAIN_H */
